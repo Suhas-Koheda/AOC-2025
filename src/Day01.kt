59dc4248b07ac9ab5636a4d13ecc84db28671f70
Unknown Haas <sharmasuhas450@gmail.com>
2025-12-07 18:32:46 +0530

    Day 1 Code

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var start=50;
+        return input.count{
+            start=if(it[0]=='L'){
+                (start-it.drop(1).toInt()).mod(100);
+            }
+            else{
+                (start+it.drop(1).toInt()).mod(100);
+            }
+            start==0;
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var start=50;
+        var count=0;
+        input.forEach{
+            val no=it.drop(1).toInt()
+            for(i in 1..no){
+                start=if(it[0]=='L') (start-1)%100 else (start+1)%100
+                if(start==0)count++;
+            }
+        };
+        return count;
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
